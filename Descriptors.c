ba91987fd73f9292b6813861fbb4bf655778c7af
CrazyRedMachine <36906596+CrazyRedMachine@users.noreply.github.com>
2021-04-11 13:14:41 +0200

    Update Descriptors.c

--- Descriptors.c
+++ Descriptors.c
@@ -247,8 +247,8 @@ const USB_Descriptor_Device_t PROGMEM DeviceDescriptor =
 
 	.Endpoint0Size          = FIXED_CONTROL_ENDPOINT_SIZE,
 
-	.VendorID               = 0x0573,
-	.ProductID              = 0x0001,
+	.VendorID               = 0x1ccf,
+	.ProductID              = 0x8048,
 	.ReleaseNumber          = VERSION_BCD(0,0,1),
 
 	.ManufacturerStrIndex   = STRING_ID_Manufacturer,
